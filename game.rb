760b0a4990c6897a250afa4015d8e7a93fd8bb74
Robert Leon <llexi@hotmail.com>
2014-06-02 15:59:34 +0100

    added game over screen for first player to 3 points with options to return to the main menu or quit

--- game.rb
+++ game.rb
@@ -14,10 +14,9 @@ class Pong < Gosu::Window
 		# Creating window, setting gamestate and preparing assets
 		super(WIDTH, HEIGHT, false)
 		@game_in_progress = false
+		@game_won = false
 		@ball = Ball.new
 		@font = Gosu::Font.new(self, "assets/victor-pixel.ttf", 40)
-		@left_score = 0
-		@right_score = 0
 	# Game Soundtrack
 	@soundtrack = [] 
 	@soundtrack << Gosu::Song.new("assets/audio/cycles.mp3")
@@ -35,9 +34,13 @@ class Pong < Gosu::Window
     def title_screen
 	    @left_player = Player.new(:left, true)
 		@right_player = Player.new(:right, true)
+	    @game_in_progress = false
     end  
 
     def setup_game(number_of_players)
+    	@game_won = false
+        @left_score = 0
+		@right_score = 0
 		if number_of_players == 2
 			@left_player = Player.new(:left, false)
 			@right_player = Player.new(:right, false)
@@ -51,11 +54,16 @@ class Pong < Gosu::Window
 
 
 	def draw
+		@ball.draw(self)
+		@left_player.draw(self)
+		@right_player.draw(self)
 		unless @game_in_progress
 	    @font.draw("PONG", 100, 100, 50, 3.0, 3.0, Gosu::Color::rgb(48,242,225))
 	    @font.draw("press '1' for single player", 150, 250, 50, 1, 1, Gosu::Color::rgb(242,128,48))
 	    @font.draw("press '2' for multiplayer", 150, 275, 50, 1, 1, Gosu::Color::rgb(242,128,48))
 	    end
+	    return unless @game_in_progress
+        game_over
 		@ball.draw(self)
 		@left_player.draw(self)
 		@right_player.draw(self)
@@ -63,8 +71,28 @@ class Pong < Gosu::Window
 		@font.draw(@right_score, WIDTH-53, 15, 0, 1, 1, Gosu::Color::rgb(48,242,128))
 	end
 
+	def game_over
+		if @left_score >= 3
+	      @font.draw("Left Side Wins!", 100, 170, 50, 2.0, 2.0, Gosu::Color::rgb(242,48,65))
+	      @font.draw("press 'm' for menu", 215, 270, 50, 1, 1, 0xffffffff)
+	      @font.draw("press 'q' to quit", 215, 295, 50, 1, 1, 0xffffffff)
+	      @game_won = true
+	    end
+	    if @right_score >= 3
+	      @font.draw("Right Side Wins!", 100, 170, 50, 2.0, 2.0, Gosu::Color::rgb(242,48,65))
+	      @font.draw("press 'm' for menu", 215, 270, 50, 1, 1, 0xffffffff)
+	      @font.draw("press 'q' to quit", 215, 295, 50, 1, 1, 0xffffffff)
+	      @game_won = true
+	    end
+	end
+
 	def update
 
+        if button_down? Gosu::KbM
+        	@game_in_progress = false
+	        title_screen
+        end
+		
 		if button_down? Gosu::Kb1
         	setup_game(1) unless @game_in_progress
         end
@@ -110,15 +138,15 @@ class Pong < Gosu::Window
 		end
 
 		if @ball.off_left?
-			@miss_sample.play
-			@right_score += 1
-			@ball = Ball.new
+			@miss_sample.play unless @game_won
+			@right_score += 1 unless @game_won
+			@ball = Ball.new unless @game_won
 
 		end
 		if @ball.off_right?
-			@miss_sample.play
-			@left_score += 1
-			@ball = Ball.new
+			@miss_sample.play unless @game_won
+			@left_score += 1 unless @game_won
+			@ball = Ball.new unless @game_won
 		end
 	end
 
