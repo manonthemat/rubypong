466a4f14689caca34fd286588a56e023e40023aa
Robert Leon <llexi@hotmail.com>
2014-06-02 12:32:28 +0100

    Ball will bounce if it hits the top or bottom of the window thanks to edge bounce method

--- lib/ball.rb
+++ lib/ball.rb
@@ -7,22 +7,37 @@ class Ball
 	def initialize
 		@x = Pong::WIDTH/2
 		@y = Pong::HEIGHT/2
-		@angle = 45
+		@angle = rand(100) + 30  
 		@speed = 4
 	end
 
-	def move!
-		dx = Gosu.offset_x(angle, speed)
-		dy = Gosu.offset_x(angle, speed)
+	def dx
+		Gosu.offset_x(angle, speed) 
+	end
+	
+	def dy 
+		Gosu.offset_y(angle, speed) 
+	end
 
+	def move!
 		@x += dx
 		@y += dy
 
 		if @y < 0
-			@y =0
-			@angle = Gosu.angle(0, 0, dx, -dy)
+			@y = 0
+			edge_bounce!
+		end
+
+		if @y > Pong::HEIGHT
+			@y = Pong::HEIGHT
+			edge_bounce!
 		end
 	end
+
+	def edge_bounce!
+		@angle = Gosu.angle(0, 0, dx, -dy)	
+	end
+
 
 
 	def x1; @x - SIZE/2; end
